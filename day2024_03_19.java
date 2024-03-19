fcadb3413c3a92d7a78b07999974764302a43fa4
num <tsun.an.yang@gmail.com>
2024-03-19 17:12:48 +0800

    新增0319檔案內容

--- day2024_03_19.java
+++ day2024_03_19.java
@@ -1,10 +1,35 @@
 package trysomethihg;
 
+import java.util.*;
+
+class CRect{
+	int height;
+	int width;
+	
+	CRect(){
+		height=10;
+		width=10;
+	}
+	
+	CRect(int height,int width){
+		this.height=height;
+		this.width=width;
+	}
+}
+
 public class day2024_03_19 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc=new Scanner(System.in);
+		
+		CRect[] rect=new CRect[4];
+		
+		for(int i=0;i<rect.length;i++) {
+			rect[i]=new CRect();
+		}
+		}
 	}
 
-}
+
